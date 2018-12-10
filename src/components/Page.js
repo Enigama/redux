d521dc3b618996d4ba01b1fd19733601d618e077
Eugene <Jeka1.1@mail.ru>
2018-12-10 10:02:34 +0300

    middleware усилители

--- src/components/Page.js
+++ src/components/Page.js
@@ -2,11 +2,21 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 export class Page extends React.Component{
+  onBtnClick = (e) => {
+    const  year = +e.currentTarget.innerText
+    this.props.setYear(year)
+  }
   render() {
     const {year, photos} = this.props
     return(
-      <div>
-        <p>У тебя {photos.length}  за {year} год</p>
+      <div className='ib page'>
+        <button className='btn'	onClick={this.onBtnClick}>2018</button>
+        <button className='btn'	onClick={this.onBtnClick}>2017</button>
+        <button className='btn'	onClick={this.onBtnClick}>2016</button>
+        <button className='btn'	onClick={this.onBtnClick}>2015</button>
+        <button className='btn'	onClick={this.onBtnClick}>2014</button>
+        <h3>{year} год</h3>
+        <p>У тебя {photos.length} фото</p>
       </div>
     )
   }
@@ -15,4 +25,5 @@ export class Page extends React.Component{
 Page.propsType = {
   year: PropTypes.number.isRequired,
   photos: PropTypes.array.isRequired,
+  setYear: PropTypes.func.isRequired,
 }
--- src/container/App.js
+++ src/container/App.js
@@ -4,18 +4,14 @@ import {Page} from '../components/Page'
 import {User} from '../components/User'
 import {setYear} from "../actions/PageActions";
 import '../App.css'
-import {store} from "../store/configureStore";
 
 class App extends Component {
   render() {
-    const {user, page} = this.props
+    const {user, page, setYearAction} = this.props
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Мой топ фото</h1>
-        </header>
-        <User name={user.name}/>
+      <div className='row'>
         <Page year={page.year} photos={page.photos} setYear={setYearAction}/>
+        <User name={user.name}/>
       </div>
     )
   }
